d8bd574777e25bf5920fdd0c4b1a80f9b0479f65
Kucherenko Alexandr <alexandr.kucherenko1977@gmail.com>
2023-08-31 17:03:58 +0400

    Initial commit

---
+++ src/test/java/ru/netology/selenide/CardDeliveryOneTest.java
@@ -0,0 +1,12 @@
+package ru.netology.selenide;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryOneTest {
+}
