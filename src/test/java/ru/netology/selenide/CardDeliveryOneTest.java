04cd9123571e8881426c6e892232a451b3fc0fa3
Kucherenko Alexandr <alexandr.kucherenko1977@gmail.com>
2023-09-01 19:45:43 +0400

    failed test two

--- src/test/java/ru/netology/selenide/CardDeliveryOneTest.java
+++ src/test/java/ru/netology/selenide/CardDeliveryOneTest.java
@@ -14,24 +14,25 @@ import static com.codeborne.selenide.Selenide.*;
 
 
 public class CardDeliveryOneTest {
-    private String generateDate(int addDays,String pattern){
-    return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
+
     @Test
-    public void shouldBESuccessFullyComplited(){
+    public void shouldBESuccessFullyComplited() {
 
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Москва");
-        String currentDate = generateDate(4,"dd.MM.yyyy");
-        $("[data-test-id='date'] input").sendKeys(Keys.SHIFT,Keys.HOME,Keys.DELETE);
+        String currentDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.SHIFT, Keys.HOME, Keys.DELETE);
         $("[data-test-id='date'] input").setValue(currentDate);
         $("[data-test-id='name'] input").setValue("Пушкин Александр Сергеевич");
         $("[data-test-id='phone'] input").setValue("+79155001162");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
-         $(".notification__content")
+        $(".notification__content")
                 .shouldBe(visible, Duration.ofSeconds(15))
-                .shouldHave(exactText("Встреча успешно забронирована на" + currentDate));
+                .shouldHave(exactText("Встреча успешно забронирована на " + currentDate));
 
 
     }
